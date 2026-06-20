71664501068f9583413fd0ed8f2fd1636e5ba0a4
Manan Makhija <128885283+mikey-00@users.noreply.github.com>
2026-06-20 15:14:56 +0000

    Add basic piece movement

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from board import Board
 
 def main():
-    chess_board = Board()
-    chess_board.display()
+    board = Board()
+
+    while True:
+        board.display()
+
+        move = input(
+            "\nEnter move (e2 e4) or 'quit': "
+        )
+
+        if move.lower() == "quit":
+            break
+
+        try:
+            start, end = move.split()
+            board.move_piece(start, end)
+
+        except ValueError:
+            print("Use format: e2 e4")
 
 if __name__ == "__main__":
     main()
