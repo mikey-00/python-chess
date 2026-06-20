d649f506353ba2e256c135b637626d8b0e16dc6a
Manan Makhija <128885283+mikey-00@users.noreply.github.com>
2026-06-20 15:13:42 +0000

    Add piece classes

---
+++ pieces.py
@@ -0,0 +1,21 @@
+class Piece:
+    def __init__(self, color):
+        self.color = color
+
+class Pawn(Piece):
+    pass
+
+class Rook(Piece):
+    pass
+
+class Knight(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class King(Piece):
+    pass
