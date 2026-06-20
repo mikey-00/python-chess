24b3d7c4a024463514143b32297e30ed679ad4d3
Manan Makhija <128885283+mikey-00@users.noreply.github.com>
2026-06-20 15:26:28 +0000

    Add interactive piece movement

--- board.py
+++ board.py
@@ -12,7 +12,7 @@ class Board:
         ]
 
     def display(self):
-        print("  a b c d e f g h")
+        print("\n  a b c d e f g h")
 
         for i in range(8):
             print(8 - i, end=" ")
@@ -21,3 +21,22 @@ class Board:
                 print(piece, end=" ")
 
             print()
+
+    def position_to_index(self, pos):
+        col = ord(pos[0].lower()) - ord('a')
+        row = 8 - int(pos[1])
+
+        return row, col
+
+    def move_piece(self, start, end):
+        sr, sc = self.position_to_index(start)
+        er, ec = self.position_to_index(end)
+
+        piece = self.board[sr][sc]
+
+        if piece == ".":
+            print("No piece at starting position.")
+            return
+
+        self.board[er][ec] = piece
+        self.board[sr][sc] = "."
